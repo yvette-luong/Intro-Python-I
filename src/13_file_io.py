5063921f6f2acf102d355a3cbabb506af1519edb
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2021-02-02 17:51:10 -0800

    update file and cal code

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,38 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+f = open("foo.txt")
+read_file = f.read()
+print(read_file)
+# print(f.closed)
+print(f"'f.close?' {f.closed}")
+f.close()
+print(f"'f.close?' {f.closed}")
+
+print("\n---NEW WAY---")
+with open("foo.txt") as f:
+    # this level -local to with statement
+    read_file = f.read()
+    print(read_file)
+
+print(" FILE IS CLOSED ")
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
+
+# "w" over write data
+# "a" append data
+# "x" create files
+# "r" for reading files
 # YOUR CODE HERE
+
+with open("bar.txt", "w") as file_input:
+    file_input.write("Arbitrary line 1\n")
+    file_input.write("Arbitrary line 2\n")
+    file_input.write("Arbitrary line 3\n")
+with open("bar.txt", "r") as file_output:
+    print("bar.txt after creation")
+    print(file_output.read())
--- src/14_cal.py
+++ src/14_cal.py
@@ -9,11 +9,16 @@ and does the following:
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
+    - default arguments 
+        `def func(param_1=0, param_2=1):` 
+
  - If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
+
  - If the user specifies two arguments, assume they passed in
    both the month and the year. Render the calendar for that
    month and year.
+
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
@@ -26,7 +31,29 @@ This would mean that from the command line you would call `python3 14_cal.py 4 2
 print out a calendar for April in 2015, but if you omit either the year or both values, 
 it should use today’s date to get the month and year.
 """
+# if no input: default to current month, current year
+# if one arg, take that as a month. default to current year
+# if two args, assume they passed in month & year
 
 import sys
 import calendar
 from datetime import datetime
+
+date = datetime.today() # return current datetime
+print(date)
+current_month = date.month # default params
+current_year = date.year # default params 
+print(sys.argv)
+if len(sys.argv) ==1 : # passed in no args
+  month = current_month
+  year =  current_year
+elif len(sys.argv) ==2: # passed in one arg
+  month = sys.argv[1]
+  year = sys.argv[2]
+else: 
+  too_many_args_flag = True
+
+if too_many_args_flag: # passed in more than 2 args
+  print(" Too Many Arguments ")
+else:
+  print(calendar.TextCalendar().formatmonth(year, month))
